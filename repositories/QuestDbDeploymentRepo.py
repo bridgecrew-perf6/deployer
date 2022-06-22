c3c53d0b8e8d1532f6c4971abfc00a4d07016d39
Tony Popov <tony@kiko.homes>
2022-06-22 21:49:02 +0400

    feat: repo methods

--- repositories/QuestDbDeploymentRepo.py
+++ repositories/QuestDbDeploymentRepo.py
@@ -2,23 +2,47 @@ from models.QuestDbDeployment import QuestDbDeployment
 from models.QuestDbDeploymentStatus import QuestDbDeploymentStatus
 from models.k8s.K8sMetadata import K8sMetadata
 from uuid import UUID
+from typing import Callable
+from dataclasses import replace
+
+deployments: dict[UUID, QuestDbDeployment] = {}
 
 
 async def create() -> QuestDbDeployment:
-    raise Exception("Not implemented")
+    deployment = QuestDbDeployment()
+    deployments[deployment.id] = deployment
+    return deployment
 
 
 async def get(deployment_id: UUID) -> QuestDbDeployment:
-    raise Exception("Not implemented")
+    deployment = deployments[deployment_id]
+    if deployment is None: raise Exception(f"Deployment with id {deployment_id} not found")
+
+    return deployment
 
 
 async def find_by_status(status: QuestDbDeploymentStatus) -> list[QuestDbDeployment]:
-    raise Exception("Not implemented")
+    values: list[QuestDbDeployment] = list(deployments.values())
+    status_filter: Callable[[QuestDbDeployment], bool] = lambda deployment: deployment.status == status
+
+    return list(filter(status_filter, values))
 
 
 async def update_status(deployment_id: UUID, status: QuestDbDeploymentStatus) -> QuestDbDeployment:
-    raise Exception("Not implemented")
+    deployment = deployments[deployment_id]
+    if deployment is None: raise Exception(f"Deployment with id {deployment_id} not found")
+
+    updated_deployment = replace(deployment, status = status)
+    deployments[deployment_id] = updated_deployment
+
+    return updated_deployment
 
 
 async def update_metadata(deployment_id: UUID, metadata: K8sMetadata) -> QuestDbDeployment:
-    raise Exception("Not implemented")
+    deployment = deployments[deployment_id]
+    if deployment is None: raise Exception(f"Deployment with id {deployment_id} not found")
+
+    updated_deployment = replace(deployment, k8s_metadata = metadata)
+    deployments[deployment_id] = updated_deployment
+
+    return updated_deployment
